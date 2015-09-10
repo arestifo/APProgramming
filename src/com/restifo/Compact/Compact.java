88c6fce432313be5982e0ef0703e4aef3e1cf733
darkphantom934 <darkphantom934@chevychasecpu.com>
2015-09-10 12:34:02 -0400

    Restarting everything

--- src/com/restifo/Compact/Compact.java
+++ src/com/restifo/Compact/Compact.java
@@ -3,47 +3,11 @@ package com.restifo.Compact;
 import static java.lang.System.out;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Compact {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		// Populate the original array
-		int[] nums = new int[100];
-		Scanner scan = new Scanner(new File("resources/Compact/compact.txt"));
-		scan.useDelimiter("  ");
-		int aCount = 0;
-		while (scan.hasNext())
-		{
-			nums[aCount] = Integer.parseInt(scan.next());
-			aCount++;
-		}
 		
-		// Print array
-		out.print("Before: ");
-		for (int i : nums)
-		{
-			out.print(i + ", ");
-		}
-		
-		// Print compacted array out
-		out.print("\nAfter: ");
-		for (int i : compact(nums))
-		{
-			out.print(i + ", ");
-		}
-	}
-	
-	private static int[] compact(int[] array)
-	{
-		int[] ret = new int[array.length];
-		for (int i = 0, c = 0; i < array.length; i++)
-		{
-			if (array[i] != 0)
-			{
-				ret[c] = array[i];
-				c++;
-			}
-		}
-		return ret;
 	}
 }
