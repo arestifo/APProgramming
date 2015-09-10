ddc6cfa65da513e02d356f381af870a80ab06b3b
darkphantom934 <darkphantom934@chevychasecpu.com>
2015-09-10 12:28:50 -0400

    Error stahp

--- src/com/restifo/Compact/Compact.java
+++ src/com/restifo/Compact/Compact.java
@@ -1,19 +1,47 @@
 package com.restifo.Compact;
 
 import java.util.Scanner;
-
+import static java.lang.System.out;
 public class Compact {
 	public static void main(String[] args)
 	{
-		// Find number of lines
-		Scanner lineScan = new Scanner("resources/Compact/compact.txt");
-		lineScan.useDelimiter("  ");
-		int count = 0;
-		while (lineScan.hasNext())
+		// Populate the original array
+		int[] nums = new int[100];
+		Scanner scan = new Scanner("resources/Compact/compact.txt");
+		scan.useDelimiter("  ");
+		int aCount = 0;
+		while (scan.hasNext())
 		{
-			lineScan.next();
-			count++;
+			nums[aCount] = Integer.parseInt(scan.next())
+			aCount++;
 		}
-
+		
+		// Print array
+		out.print("Before: ");
+		for (int i : nums)
+		{
+			out.print(i + ", ");
+		}
+		
+		// Print compacted array out
+		out.print("\nAfter: ");
+		for (int i : compact(nums))
+		{
+			out.print(i + ", ");
+		}
+	}
+	
+	private static int[] compact(int[] array)
+	{
+		int[] ret = new int[array.length];
+		for (int i = 0, c = 0; i < array.length; i++)
+		{
+			if (array[i] != 0)
+			{
+				ret[c] = array[i];
+				c++;
+			}
+		}
+		return ret;
 	}
 }
