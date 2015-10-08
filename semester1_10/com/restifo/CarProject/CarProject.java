e6c8026d9c8cc6ff4c68789d6aa9b6d9a93ddc51
darkphantom934 <calix1999@gmail.com>
2015-10-08 12:25:00 -0400

    Finished project CarProject

---
+++ semester1_10/com/restifo/CarProject/CarProject.java
@@ -0,0 +1,25 @@
+package com.restifo.CarProject;
+import static java.lang.System.out;
+
+import java.util.Scanner;
+public class CarProject {
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		String make, model, license;
+		out.print("Enter the make of your car: ");
+		make = scan.nextLine();
+		out.print("Enter the model of your car: ");
+		model = scan.nextLine();
+		out.print("Enter the license plate: ");
+		license = scan.nextLine();
+		
+		int num = license.charAt(0) + license.charAt(1) + license.charAt(2) + Integer.parseInt(license.substring(4));
+		char letter = (char) (65 + (num % 26));		
+		String rental = String.valueOf(letter) + num;
+		
+		out.println("\nMake = " + make);
+		out.println("Model = " + model);
+		out.printf("%s = %s", license, rental);
+	}
+}
