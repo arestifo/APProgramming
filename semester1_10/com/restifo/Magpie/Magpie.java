aad7defa135ea72f52d789176e66d7f64b57c659
darkphantom934 <calix1999@gmail.com>
2015-10-13 12:40:17 -0400

    Finished magpie

--- semester1_10/com/restifo/Magpie/Magpie.java
+++ semester1_10/com/restifo/Magpie/Magpie.java
@@ -32,21 +32,20 @@ public class Magpie
 	public String getResponse(String statement)
 	{
 		String response = "";
-		if (statement.indexOf("no") >= 0)
-		{
+		if (statement.trim().length() == 0)
+			response = "Say something please.";
+		else if (statement.indexOf("no") >= 0)
 			response = "Why so negative?";
-		}
-		else if (statement.indexOf("mother") >= 0
-				|| statement.indexOf("father") >= 0
-				|| statement.indexOf("sister") >= 0
-				|| statement.indexOf("brother") >= 0)
-		{
+		else if (statement.indexOf("mother") >= 0 || statement.indexOf("father") >= 0 || statement.indexOf("sister") >= 0 || statement.indexOf("brother") >= 0)
 			response = "Tell me more about your family.";
-		}
+		else if (statement.indexOf("dog") >= 0 || statement.indexOf("cat") >= 0)
+			response = "Tell me more about your pets";
+		else if (statement.indexOf("Mr. Nateghi") >= 0)
+			response = "He sounds like a good teacher.";
+		else if (statement.indexOf("dank memes") >= 0 || statement.indexOf("memes") >= 0 || statement.indexOf("meme") >= 0)
+			response = "Hipppppppp";
 		else
-		{
 			response = getRandomResponse();
-		}
 		return response;
 	}
 
@@ -56,7 +55,7 @@ public class Magpie
 	 */
 	private String getRandomResponse()
 	{
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -77,7 +76,15 @@ public class Magpie
 		{
 			response = "You don't say.";
 		}
-
+		else if (whichResponse == 4)
+		{
+			response = "Wanna see my meme collection?";
+		}
+		else if (whichResponse == 5)
+		{
+			response = "I'm watching you.";
+		}
+		
 		return response;
 	}
 }
