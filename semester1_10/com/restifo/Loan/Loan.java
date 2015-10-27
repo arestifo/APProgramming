f5076caf027630ac88fe8d881a2d87fe86a78f78
darkphantom934 <calix1999@gmail.com>
2015-10-27 12:43:47 -0400

    Working on Loan

--- semester1_10/com/restifo/Loan/Loan.java
+++ semester1_10/com/restifo/Loan/Loan.java
@@ -1,9 +1,25 @@
 package com.restifo.Loan;
+import static java.lang.System.out;
 
+import java.util.Scanner;
 public class Loan {
 
 	public static void main(String[] args)
 	{
-		
+		Scanner scan = new Scanner(System.in);
+		out.print("Amount of the loan: ");
+		double amt = scan.nextDouble();
+		out.print("Length of loan in years: ");
+		int years = scan.nextInt();
+		out.print("Low interest rate in %: ");
+		double low = scan.nextDouble();
+		out.print("High interest rate in %: ");
+		double high = scan.nextDouble();
+		double count = low - high;
+		while (count < 0)
+		{
+			double c = Math.pow(arg0, )
+			count += 0.25;
+		}
 	}
 }
