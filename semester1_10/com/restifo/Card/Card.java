bfa077570083f822d99051503fdf0e0de05a7372
darkphantom934 <calix1999@gmail.com>
2015-11-13 12:13:16 -0500

    Forgot the files

---
+++ semester1_10/com/restifo/Card/Card.java
@@ -0,0 +1,37 @@
+package com.restifo.Card;
+public class Card {
+
+	private String suit;
+	private String rank;
+	private int pointValue;
+
+	public Card(String cardRank, String cardSuit, int cardPointValue) 
+	{
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	}
+	
+	public String suit()
+	{
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	}
+
+	public String rank()
+	{
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	}
+
+	public int pointValue()
+	{
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	}
+
+	public boolean matches(Card otherCard) 
+	{
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	}
+	
+	public String toString()
+	{
+		
+	}
+}
---
+++ semester1_10/com/restifo/Card/CardTester.java
@@ -0,0 +1,7 @@
+package com.restifo.Card;
+public class CardTester {
+	public static void main(String[] args) 
+	{
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+	}
+}
