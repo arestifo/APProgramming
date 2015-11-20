db4bcc012111211a3bc6bf0d454e3e1d1933446b
darkphantom934 <calix1999@gmail.com>
2015-11-20 12:40:54 -0500

    Finished banking project and updated projects.txt

---
+++ semester1_10/com/restifo/Bank/Banking.java
@@ -0,0 +1,26 @@
+package com.restifo.Bank;
+public class Banking
+{
+   public static void main (String[] args)
+   {
+      Account acct1 = new Account ("Ted Murphy", 72354, 102.56);
+      Account acct2 = new Account ("Anita Gomez", 69713, 40.00);
+      Account acct3 = new Account ("Sanchit Reddy", 93757, 759.32);
+      acct1.deposit (25.85);
+      double gomezBalance = acct2.deposit (500.00);
+      System.out.println ("Gomez balance after deposit: " + gomezBalance);
+      System.out.println ("Gomez balance after withdrawal: " + acct2.withdraw (430.75, 1.50));
+      System.out.printf("Reddy balance before moving funds: %,.2f\n", acct3.getBalance());
+      acct3 = acct2.moveMoney(10, acct3);
+      System.out.printf("Reddy balance after moving funds: %,.2f\n", acct3.getBalance());
+      acct3.withdraw (800.00, 0.0); 
+      acct1.addInterest();
+      acct2.addInterest();
+      acct3.addInterest();
+      System.out.println();
+      System.out.println(acct1);
+      System.out.println(acct2);
+      System.out.println(acct3);
+   }
+}
+
