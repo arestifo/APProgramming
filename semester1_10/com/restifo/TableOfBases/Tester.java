245ae4785da392487768e0d0b0c755579b4b74c3
darkphantom934 <calix1999@gmail.com>
2015-12-16 12:14:36 -0500

    Added TableOfBases and updated projects.txt

---
+++ semester1_10/com/restifo/TableOfBases/Tester.java
@@ -0,0 +1,18 @@
+package com.restifo.TableOfBases;
+import static java.lang.System.out;
+import static java.lang.System.err;
+import java.math.BigInteger;
+public class Tester {
+
+	public static void main(String[] args) 
+	{
+		err.printf("Decimal\t\tBinary\t\tOctal\t\tHex\t\tCharacter\n");
+		for (int i = 65; i < 91; i++)
+			out.printf("%s\t\t%s\t\t%s\t\t%s\t\t%s\n", i, toBase(i, 2), toBase(i, 8), toBase(i, 16), (char) i);
+	}
+	
+	private static String toBase(int number, int base)
+	{
+		return Integer.toString(number, base);
+	}
+}
