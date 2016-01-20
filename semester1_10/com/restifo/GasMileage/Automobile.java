c3e828c95d93f21c4a90975a07706444c4eb2b1b
darkphantom934 <calix1999@gmail.com>
2016-01-20 00:52:52 -0500

    Finished Automobile project

--- semester1_10/com/restifo/GasMileage/Automobile.java
+++ semester1_10/com/restifo/GasMileage/Automobile.java
@@ -1,5 +1,14 @@
 package com.restifo.GasMileage;
 
 public class Automobile {
-
+	private int mpg;
+	private double gallons;
+	public Automobile(int mpg)
+	{
+		this.mpg = mpg;
+		this.gallons = 0;
+	}
+	public void takeTrip(int miles){ gallons -= (miles / (double)mpg); }
+	public void fillUp(int gallons) { this.gallons += gallons; }
+	public double reportFuel() { return gallons; }
 }
--- semester1_10/com/restifo/GasMileage/Tester.java
+++ semester1_10/com/restifo/GasMileage/Tester.java
@@ -1,8 +1,13 @@
 package com.restifo.GasMileage;
-
 public class Tester {
 	public static void main(String[] args)
 	{
+		Automobile myBmw = new Automobile(24);
 		
+		myBmw.fillUp(20);
+		myBmw.takeTrip(100);
+		
+		double fuel_left = myBmw.reportFuel();
+		System.out.println(fuel_left);
 	}
 }
