a27d47cc3781631ffb9ee3678f29fcb12c8729e2
darkphantom934 <calix1999@gmail.com>
2016-03-03 09:49:33 -0500

    Meh I'll just do it at home

---
+++ semester2_10/com/restifo/ShoppingCart/Shop.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import static java.lang.System.out;
+public class Shop
+{
+	public static void main (String[] args)
+	{
+		Item item;
+		String itemName;
+		double itemPrice;
+		int quantity;
+		Scanner scan = new Scanner(System.in);
+		String keepShopping = "y";
+		do
+		{
+			out.print ("Enter the name of the item: ");
+			itemName = scan.nextLine();
+			out.print ("Enter the unit price: ");
+			itemPrice = scan.nextDouble();
+			out.print ("Enter the quantity: ");
+			quantity = scan.nextInt();
+			// *** create a new item and add it to the cart
+			// *** print the contents of the cart object using println
+			out.print("Continue shopping (y/n)? ");
+			keepShopping = scan.nextLine();
+		}
+		while (keepShopping.equals("y"));
+	}
+}
