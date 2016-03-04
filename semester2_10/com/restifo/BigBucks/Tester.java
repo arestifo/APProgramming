e4e9f2d39e3c5e80bb5826db74cedcaddec04f80
darkphantom934 <calix1999@gmail.com>
2016-03-04 09:59:58 -0500

    Finished BigBucks

---
+++ semester2_10/com/restifo/BigBucks/Tester.java
@@ -0,0 +1,40 @@
+package com.restifo.BigBucks;
+
+import static java.lang.System.out;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Tester {
+
+	public static void main(String[] args) 
+	{
+		Scanner scan = new Scanner(System.in);
+		String name = "";
+		double deposit = 0.0;
+		ArrayList<BankAccount> accounts = new ArrayList<BankAccount>();
+		while (true)
+		{
+			out.print("Please enter the name to whom the account belongs. (\"Exit\" to abort): ");
+			name = scan.nextLine();
+			if (name.toLowerCase().equals("exit")) break;
+			out.print("Please enter the amount of the deposit: ");
+			deposit = Double.parseDouble(scan.nextLine());
+			accounts.add(new BankAccount(name, deposit));
+		}
+		
+		double max = 0.0;
+		String maxName = "";
+		for (BankAccount b : accounts)
+		{
+			if (b.balance > max)
+			{
+				max = b.balance;
+				maxName = b.name;
+			}
+		}
+		out.printf("The account with the largest balance belongs to %s.\n", maxName);
+		out.printf("The amount is $%,.2f", max);
+		
+		scan.close();
+	}
+}
