e5ea9b40a5472792ead197bcbf5655e40050b236
darkphantom934 <calix1999@gmail.com>
2016-04-08 09:47:15 -0400

    Formatting

--- semester2_10/com/restifo/TestQuestions/MultChoice.java
+++ semester2_10/com/restifo/TestQuestions/MultChoice.java
@@ -11,14 +11,17 @@ public class MultChoice extends TestQuestion {
 	{
 		answer = new String[data.length - 1];
 		question = data[0];
-		for(int i = 1; i < data.length; i++){
+		for(int i = 1; i < data.length; i++)
+		{
 			answer[i - 1] = data[i];
 		}
 	}
-	public String toString(){
+	public String toString()
+	{
 		String pain = "";
 		pain += question + "\n";
-		for(int i = 0; i < answer.length; i++){
+		for(int i = 0; i < answer.length; i++)
+		{
 			pain += (char)('A' + i) + ")" + answer[i] + "\n";
 		}
 		return pain;
