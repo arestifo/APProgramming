20dd77e437dc7eb40cdf9f29d21d0060b7a42b19
darkphantom934 <calix1999@gmail.com>
2016-04-19 09:42:11 -0400

    Minified code

--- semester2_10/com/restifo/Fibonacci/Fibonacci.java
+++ semester2_10/com/restifo/Fibonacci/Fibonacci.java
@@ -1,31 +1,27 @@
 package com.restifo.Fibonacci;
-
+import static java.lang.System.out;
 public class Fibonacci {
 	public static void main(String[] args)
 	{
-		System.out.println(recFibonacci(8));
-		System.out.println(nonRecFibonacci(8));
+		out.println(recFibonacci(8));
+		out.println(nonRecFibonacci(8));
 	}
 	
 	private static int recFibonacci(int n)
 	{
 		if (n < 2)
-		{
 			return 1;
-		}
 		else
-		{
 			return recFibonacci(n - 1) + recFibonacci(n - 2);
-		}
 	}
 	
 	private static int nonRecFibonacci(int n)
 	{
-		int[] temp = new int[n]; temp[0] = 1; temp[1] = 1;
+		int[] temp = new int[n];
+		for (int i = 0; i < 2; i++)
+			temp[i] = 1;
 		for (int i = 2; i < n; i++)
-		{
 			temp[i] = temp[i - 1] + temp[i - 2];
-		}
 		return temp[n - 1] + temp[n - 2];
 	}
 }
