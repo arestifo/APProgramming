ad0cf81a81d4b2e75ead73caf64a58ee02a0531c
darkphantom934 <calix1999@gmail.com>
2016-04-19 09:46:36 -0400

    bettar

--- semester2_10/com/restifo/Fibonacci/Fibonacci.java
+++ semester2_10/com/restifo/Fibonacci/Fibonacci.java
@@ -3,23 +3,24 @@ import static java.lang.System.out;
 public class Fibonacci {
 	public static void main(String[] args)
 	{
-		out.println(recFibonacci(8));
-		out.println(nonRecFibonacci(8));
+		out.println(rf(8));
+		out.println(nrf(8));
 	}
 	
-	private static int recFibonacci(int n)
+	// Recursive fibonacci
+	private static int rf(int n)
 	{
 		if (n < 2)
-			return 1;
+			return n;
 		else
-			return recFibonacci(n - 1) + recFibonacci(n - 2);
+			return rf(n - 1) + rf(n - 2);
 	}
 	
-	private static int nonRecFibonacci(int n)
+	// Non-recursive fibonacci
+	private static int nrf(int n)
 	{
 		int[] temp = new int[n];
-		for (int i = 0; i < 2; i++)
-			temp[i] = 1;
+		temp[1] = 1;
 		for (int i = 2; i < n; i++)
 			temp[i] = temp[i - 1] + temp[i - 2];
 		return temp[n - 1] + temp[n - 2];
